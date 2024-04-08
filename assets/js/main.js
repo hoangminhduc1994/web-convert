ca986b62c327e775eba97c3529127f57d03ad51e
hoangminhduc1994 <151418180+hoangminhduc1994@users.noreply.github.com>
2024-04-08 09:06:47 +0700

    update-10

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,30 @@
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:true,
+ autoplay:true,
+ autoplayTimeout:2000,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:4
+      },
+      1000:{
+          items:4
+      }
+  }
+})
+
+
+
 $(document).ready(function(){
     $(".owl-carousel").owlCarousel();
-  });
+    $(".fa-chevron-left").click(function(){
+      $(".owl-carousel").trigger("prev.owl.carousel");
+    });
+  
+    $(".fa-chevron-right").click(function(){
+      $(".owl-carousel").trigger("next.owl.carousel");
+    });})
